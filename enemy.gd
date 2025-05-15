07a929a7a52f70d441fe94c0aefb371d02726988
Travis Henderson <TravisHenderson646@gmail.com>
2025-05-15 17:55:58 -0400

    PROPER PIXEL COLLISION, FIXED WALLHUGGING AND COLLISION SHAPE JITTER

--- enemy.gd
+++ enemy.gd
@@ -1,7 +1,9 @@
 extends CharacterBody2D
 
 
-var gravity:= 200.0
+var gravity:= 3.33
+@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+@onready var polygon_2d: Polygon2D = $Polygon2D
 
 func _physics_process(_delta: float) -> void:
 	# Add the gravity.
--- player.gd
+++ player.gd
@@ -4,14 +4,14 @@ extends CharacterBody2D
 @export_category("Movement Stats")
 @export_range(30, 50, .1) var SPEED = 40.0
 @export_range(-110, -95, .1) var JUMP_VELOCITY = -100.0
-@export_range(180, 230, 1) var gravity:= 200.0
+@export_range(3.2, 3.5, .01) var gravity:= 3.33
 
 @onready var remote_transform_2d: RemoteTransform2D = $RemoteTransform2D
 
 
 func _physics_process(_delta: float) -> void:
 	# Add the gravity.
-	velocity.y += gravity /60
+	velocity.y += gravity
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
