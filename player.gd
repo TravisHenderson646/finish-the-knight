1f3d21c20ef2b51b63e42601ab19ea119ef57971
Travis Henderson <TravisHenderson646@gmail.com>
2025-05-21 11:23:50 -0400

    simple dash

--- player.gd
+++ player.gd
@@ -8,23 +8,51 @@ extends CharacterBody2D
 
 @onready var remote_transform_2d: RemoteTransform2D = $RemoteTransform2D
 
+var can_jump := true
+
+var can_dash := true
+var dash_remaining_ticks := 0
+var dash_duration := 10
+var dash_direction = 0
+var dash_speed = 150.0
+
 
 func _physics_process(_delta: float) -> void:
 	# Add the gravity.
 	velocity.y += gravity
+
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed('jump') and can_jump:
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis('left', 'right')
 	if direction:
 		velocity.x = direction * SPEED
+		if dash_remaining_ticks == 0:
+			if velocity.x > 0:
+				dash_direction = 1
+			else:
+				dash_direction = -1
 	else:
 		velocity.x = 0
 
+	# dash
+	if Input.is_action_just_pressed('dash') and can_dash:
+		dash_remaining_ticks = dash_duration
+		can_jump = false
+		can_dash = false
+	if dash_remaining_ticks > 0:
+		dash_remaining_ticks -= 1
+		velocity.x = dash_direction * dash_speed
+		velocity.y = 0
+	print(dash_remaining_ticks > 0)
+
+
 	move_and_slide()
-	# prevent being 1 pixel into floor
-	#if is_on_floor():
-		#position.y = round(position.y)
+
+	if is_on_floor():
+		if dash_remaining_ticks != 0:
+			return
+		can_jump = true
+		can_dash = true
